9fcabb97e0da5d55c34ad8ef0ed711c21f5e7c80
Kashif <kashif147@gmail.com>
2024-10-16 17:20:48 +0100

    changes in allowed origins and expirytime

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
  // Cross origin Resource Sharing
  const allowedOrigins = [
-    'https://www.yoursite.com', 
-    'https://nodsjsapi.northeurope.cloudapp.azure.com:3500',
-    'http://127.0.0.1:27017',
-     'http://localhost:3500'
+   //  'https://www.yoursite.com', 
+    'https://nodsjsapi.northeurope.cloudapp.azure.com',
+   //  'http://127.0.0.1:27017',
+     'http://localhost:3500',
+      'http://localhost:3000'
     ];
 
     module.exports = allowedOrigins;
