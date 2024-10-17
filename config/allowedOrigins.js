f01a266fc732bc79466e8014c6d0a6d543c4492b
shahnazkashif <shahnaz.azizm@gmail.com>
2024-10-17 22:49:04 +0100

    kashif

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
  // Cross origin Resource Sharing
  const allowedOrigins = [
     'https://www.yoursite.com', 
-   //  'https://nodsjsapi.northeurope.cloudapp.azure.com:3500',
+    'https://nodsjsapi.northeurope.cloudapp.azure.com',
     'http://127.0.0.1:27017',
      'http://localhost:3500',
      'http://localhost:3000'
