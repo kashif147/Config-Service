f05c06542729682ccbac82921e0f9f0b3aef7abd
Kashif <kashif147@gmail.com>
2024-10-25 18:47:45 +0100

    regionprofile deletion

--- controllers/regionController.js
+++ controllers/regionController.js
@@ -138,41 +138,6 @@ const updateRegion = async (req, res) => {
         }      
     }
 
-    //regionProfile
-
-    const createRegionProfile =  async (req, res) => {
-        const session = await mongoose.startSession();
-        session.startTransaction();
-    
-        try {
-    
-            // Step 1: Create Profile
-            const region = await Region.create([req.body.region], { session });
-        
-            const profileId = region[0]._id;
-            console.log(profileId);
-    
-            // Step 2: Create Related Entities
-           // const contacts = req.body.contacts.map(contact => ({ ...contact, profileId }));
-           // const regionalcontacts = req.body.regionalcontacts.map(rgcontact => ({ ...rgcontact, profileId }));
-        
-            //await contacts.create(contacts, { session });
-            //await regionalcontacts.create(regionalcontacts, { session });
-        
-            // Step 3: Commit transaction if all went well
-            await session.commitTransaction();
-            session.endSession();
-        
-            res.status(201).send({ profileId });
-          } catch (error) {
-            // Rollback transaction
-            await session.abortTransaction();
-            session.endSession();
-            res.status(500).send({ error: 'Profile registration failed', details: error });
-          }
-    };
-
-    
     module.exports = {
         getAllRegions,
         createNewRegion,
