e7cc0bfd8f540beea41cc20c73364dfddd275dc7
Kashif <kashif147@gmail.com>
2024-11-01 22:30:30 +0000

    url changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@
  const allowedOrigins = [
     'https://www.yoursite.com', 
     'https://nodsjsapi.northeurope.cloudapp.azure.com',
+    'https://node-api-app-dxecgpajapacc4gs.northeurope-01.azurewebsites.net',
     'http://127.0.0.1:27017',
      'http://localhost:3500',
      'http://localhost:3000'
