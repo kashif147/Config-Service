c564f425c20c477ae892819b755fba224292f396
sadeeq <sadeequk8@gmail.com>
2025-04-05 17:24:15 +0500

    📌 commented dbconn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose');
-console.log('testing connection')
+const mongoose = require("mongoose");
+console.log("testing connection");
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            // useUnifiedTopology: true,
-            // useNewUrlParser: true
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      // useUnifiedTopology: true,
+      // useNewUrlParser: true
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-module.exports = connectDB
-
-
-
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const dotenv = require("dotenv");
 //require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions')
-const { logger } = require('./middleware/logEvents');
-const  errorHandler  = require('./middleware/errorHandler');
-const verifyJWT = require('./middleware/verifyJWT');
-const cookieParser = require('cookie-parser');
-const credentials = require('./middleware/credentials');
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3500;
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
+const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 //const { swaggerUi, swaggerDocs } = require('./swagger');
@@ -36,67 +36,57 @@ app.use(cors(corsOptions));
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
 
 //middleware for cookies
 app.use(cookieParser());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use('/', require('./routes/root'));
-app.use('/register', require('./routes/register'));
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 app.use(verifyJWT);
-app.use('/employees', require('./routes/api/employees'));
-app.use('/regiontype', require('./routes/api/regiontype'));
-app.use('/contactType', require('./routes/api/contactType'));
-app.use('/region', require('./routes/api/region'));
-app.use('/contact', require('./routes/api/contact'));
-app.use('/lookuptype', require('./routes/api/lookuptype'));
-app.use('/lookup', require('./routes/api/lookup'));
-app.use('/partner', require('./routes/api/PartnerRoutes'));
-app.use('/children', require('./routes/api/ChildrenRoutes'));
+app.use("/employees", require("./routes/api/employees"));
+app.use("/regiontype", require("./routes/api/regiontype"));
+app.use("/contactType", require("./routes/api/contactType"));
+app.use("/region", require("./routes/api/region"));
+app.use("/contact", require("./routes/api/contact"));
+app.use("/lookuptype", require("./routes/api/lookuptype"));
+app.use("/lookup", require("./routes/api/lookup"));
+app.use("/partner", require("./routes/api/PartnerRoutes"));
+app.use("/children", require("./routes/api/ChildrenRoutes"));
 
-app.use("/profile", require('./routes/api/ProfileRoutes'))
-app.use("/gardaLifeClaims", require('./routes/api/GardaLifeClaimRoutes'))
-app.use("/partnerLifeClaims", require('./routes/api/PartnerLifeClaimRoute'))
-app.use("/criticalIllness", require('./routes/api/CriticalIllnessClimRoutes'))
-app.use("/transferRequest", require('./routes/api/TransferRequestRoutes'))
-
-
-
-
+app.use("/profile", require("./routes/api/ProfileRoutes"));
+app.use("/gardaLifeClaims", require("./routes/api/GardaLifeClaimRoutes"));
+app.use("/partnerLifeClaims", require("./routes/api/PartnerLifeClaimRoute"));
+app.use("/criticalIllness", require("./routes/api/CriticalIllnessClimRoutes"));
+app.use("/transferRequest", require("./routes/api/TransferRequestRoutes"));
 
 //app.all is for routin and apply for all http methods at once
 //app.use is for middleware and not support rjax
- app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));        
-    } else if (req.accepts('json')) {
-        res.json({ error: "404 Not Found"});        
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
- });
- 
- app.use(errorHandler);
- mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running 
-        on port ${PORT}`));    
-})
- 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
-
-
-
-
- 
- 
+app.use(errorHandler);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () =>
+    console.log(`Server running 
+        on port ${PORT}`)
+  );
+});
