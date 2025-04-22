7ae9ee224e253957a76f2a4c5a70114c297439d2
sadeeq <sadeequk8@gmail.com>
2025-04-23 00:55:49 +0500

    📌 default image added

--- routes/auth.js
+++ routes/auth.js
@@ -26,37 +26,5 @@ const authController = require("../controllers/authController");
  */
 
 router.post("/", authController.handleLogin);
-router.post("/microsoft", authController.handleMicrosoftCallback);
 
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const authController = require("../controllers/authController");
-
-// /**
-//  * @swagger
-//  * /auth:
-//  *   post:
-//  *     summary: Get access token
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             type: object
-//  *             properties:
-//  *               user:
-//  *                 type: string
-//  *                 example: 'username'
-//  *               pwd:
-//  *                 type: string
-//  *                 example: 'password'
-//  *     responses:
-//  *       200:
-//  *         description: Bearer token
-//  */
-
-// router.post("/", authController.handleLogin);
-
-// module.exports = router;
